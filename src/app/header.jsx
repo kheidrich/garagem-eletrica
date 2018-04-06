553b57e0aeeb0681f1cedeceacd99d7a898daa93
kheidrich <kevin.pinzon@ulbra.br>
2018-04-06 11:34:06 -0300

    fixed misspelled property

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -7,7 +7,7 @@ class Header extends React.Component {
             <nav>
                 <div className="nav-wrapper black">
                     <div className="center">
-                        <img src={raceFlagIcon} width="100" heigh="100" class="race-flag-icon"/>
+                        <img src={raceFlagIcon} width="100" heigh="100" className="race-flag-icon"/>
                         <a href="#" className="brand-logo center belair-font"><b>Garagem Eletrica</b></a>
                     </div>
                 </div>
