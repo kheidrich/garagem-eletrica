a763c449ebc405d90306695e7551a2915f0edd61
kheidrich <kevin.pinzon@ulbra.br>
2018-04-11 13:21:44 -0300

    create Divider an Section components

---
+++ src/elements/layout/divider.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Divider extends Component {
+    render() {
+        return (
+            <div className="divider"></div>
+        )
+    }
+}
