60d792064b13bae22e9742751499a6fced8bbac2
kheidrich <kevin.pinzon@ulbra.br>
2018-04-11 10:12:46 -0300

    add Material Icons

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ import 'materialize-css';
 import 'materialize-css/dist/css/materialize.css';
 
 import './style.css';
+import './assets/material-icons.css';
 
 if(module.hot)
     module.hot.accept();
