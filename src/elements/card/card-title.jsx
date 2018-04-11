b306a3ef71da2986ce523f7ea8bcbc800cc7df3f
kheidrich <kevin.pinzon@ulbra.br>
2018-04-11 10:46:11 -0300

    add center option to CardTitle

--- src/elements/card/card-title.jsx
+++ src/elements/card/card-title.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 class CardTitle extends Component {
     render() {
       return (
-        <span className="card-title">{this.props.children}</span>
+        <span className={`card-title ${this.props.center ? 'center' : ''}`}>{this.props.children}</span>
       );
+    }
+
+    static propTypes = {
+      center: PropTypes.bool
     }
 }
 
