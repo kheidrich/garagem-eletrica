760d2e05554a14897785110437cac0bb4ae7dcba
Kevin Pinzon <kevin.pinzon@hotmail.com>
2019-11-08 15:03:12 -0300

    add sass do project

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -30,6 +30,19 @@ module.exports = {
                 }
             },
             {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('node-sass'),
+                        },
+                    },
+                ],
+            },
+            {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             },
